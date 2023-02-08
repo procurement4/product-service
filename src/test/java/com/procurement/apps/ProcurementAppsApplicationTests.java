93f9966bbc1fff45d022eb52c6f94a8041a1e75e
hendra lw <hndralw@gmail.com>
2023-02-08 23:00:58 +0700

    first commit

---
+++ src/test/java/com/procurement/apps/ProcurementAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procurement.apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementAppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
